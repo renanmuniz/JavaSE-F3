c6d4aad5bd5ecb2521876b6d71bff3f5008df289
Renan Muniz Merlo <renan.mmerlo@gmail.com>
2020-06-16 11:38:02 -0300

    Exer1

---
+++ Aula3/src/exer1/Exer1.java
@@ -0,0 +1,21 @@
+package exer1;
+
+import javax.swing.*;
+
+
+import static javax.swing.JOptionPane.showMessageDialog;
+import static java.lang.System.out;
+import static java.lang.System.exit;
+
+public class Exer1 {
+    public static void main(String[] args) {
+        System.out.println("Ola 1");
+        out.println("Ola 2");
+
+        JOptionPane.showMessageDialog(null, "Ola 3");
+        showMessageDialog(null,"Ola 4");
+
+        System.exit(0);
+        exit(0);
+    }
+}
